01c20e7bbae69917bce14c5bb0dc607c48cfe105
Nidhin Malaviya <nidhin.malaviya1008@gmail.com>
2023-07-14 22:25:52 +0530

    initial

---
+++ Utils/featureData.js
@@ -0,0 +1,21 @@
+
+const FeatureData = [
+    {
+        "image": "/feature1.jpg",
+        "title": "FEATURE 1",
+        "discription": "Talk about some of the details of your offer with a focus on the value people get back."
+    },
+    {
+        "image": "/feature2.jpg",
+        "title": "FEATURE 2",
+        "discription": "Talk about some of the details of your offer with a focus on the value people get back."
+    },
+    {
+        "image": "/feature3.jpg",
+        "title": "FEATURE 3",
+        "discription": "Talk about some of the details of your offer with a focus on the value people get back."
+    }
+
+]
+
+export default FeatureData;
---
+++ Utils/scheduleData.js
@@ -0,0 +1,21 @@
+
+const ScheduleData = [
+    {
+        "image": "/monitor.svg",
+        "title": "Benefit 1",
+        "discription": "Highlight the benefits of signing up for an appointment, online class, or video consultation."
+    },
+    {
+        "image": "/heart.svg",
+        "title": "Benefit 2",
+        "discription": "Highlight the benefits of signing up for an appointment, online class, or video consultation."
+    },
+    {
+        "image": "/star.svg",
+        "title": "Benefit 3",
+        "discription": "Highlight the benefits of signing up for an appointment, online class, or video consultation."
+    }
+
+]
+
+export default ScheduleData;
